4ad4a311485bdadc3061df1c0d0920f2b94c33eb
lmfrench <lisa_french@hotmail.com>
2022-12-27 15:27:17 +0000

    add getComputerChoice function

--- rps-script.js
+++ rps-script.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+// Create function for computer to select Rock, Paper or Scissors
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+    let compChoice;
+    if (choice === 0){
+        compChoice = "Rock";
+    } else if (choice === 1){
+        compChoice = "Paper";
+    }else{
+        compChoice = "Scissors";
+     }
+     return compChoice;
+}
+
+let test = getComputerChoice();
+
+console.log(test);
