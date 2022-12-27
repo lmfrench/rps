6bcb26365676ff634cafbffce07217ac3bae668b
lmfrench <lisa_french@hotmail.com>
2022-12-27 16:45:50 +0000

    Add game function

--- rps-script.js
+++ rps-script.js
@@ -20,29 +20,66 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
 
-    //standardise case from playerSelection
-    let playerChoice = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    
     // Stores who will win
     let outcome;
 
     //Logic for the game
-    if (playerChoice === "Rock" && computerSelection === "Paper"
-        || playerChoice === "Paper" && computerSelection === "Scissors"
-        || playerChoice === "Scissors" && computerSelection === "Rock" )
+    if (playerSelection === "Rock" && computerSelection === "Paper"
+        || playerSelection === "Paper" && computerSelection === "Scissors"
+        || playerSelection === "Scissors" && computerSelection === "Rock" )
         {
-        outcome = "You Lose! " + computerSelection + " beats " + playerChoice;
+        outcome = "Player Loses";
     }
-    else if (playerChoice === computerSelection){
-        outcome = "You Draw! The Player's choice (" + playerChoice + ") and Computer's choice (" + computerSelection + ") are the same!";
+    else if (playerSelection === computerSelection){
+        outcome = "Player Draws";
     }
     else {
-        outcome = "You Win! " + playerChoice + " beats " + computerSelection;
+        outcome = "Player Wins";
     }
     //Return outcome of game
     return outcome;
 }
 
-let computerRPS = getComputerChoice();
 
-console.log(playRound("rOck", "Scissors"));
+// Function to play 5 rounds
+function game(){
+
+// Variable to store score
+    let overallScore = 0;
+
+// Play rock paper scissors 5 times
+    for (let round = 0; round < 5; round++){
+        // Computer choses
+        let computerSelection = getComputerChoice();
+
+        // Ask player to choose
+        let playerChoice = prompt("Round " + round + "/5 : Rock, Paper, Scissors?");
+        
+        // Standardise case from playerChoice
+        let playerSelection = playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
+   
+        // Play the round
+        let roundOutcome = playRound(playerSelection, computerSelection);
+
+        // Tell player the outcome of the round
+        
+        if (roundOutcome === "Player Loses"){
+            alert("You Lose! " + computerSelection + " beats " + playerSelection + ". Current Score: " + overallScore);
+        }
+        else if (roundOutcome === "Player Draws"){
+            alert("You Draw! The Player's choice (" + playerSelection + ") and Computer's choice (" + computerSelection + ") are the same! Current Score: " + overallScore);
+        }
+        else {
+            overallScore++;
+            alert("You Win! " + playerSelection + " beats " + computerSelection + ". Current Score: " + overallScore);
+        }
+    }
+
+    return overallScore;
+
+}
+
+let score = game();
+
+// tell player final score
+alert("Final Score: You won " + score + " rounds.");
