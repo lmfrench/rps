aa437ed81e1a43e96fd3f870584484da239d13ac
lmfrench <lisa_french@hotmail.com>
2022-12-27 16:06:15 +0000

    Add playRound function

--- rps-script.js
+++ rps-script.js
@@ -1,18 +1,48 @@
 // Create function for computer to select Rock, Paper or Scissors
 
 function getComputerChoice(){
+    //Choose random number between 0-2
     let choice = Math.floor(Math.random() * 3);
-    let compChoice;
+    let computerChoice;
+
+    //Select Rock, Paper or Scissors
     if (choice === 0){
-        compChoice = "Rock";
+        computerChoice = "Rock";
     } else if (choice === 1){
-        compChoice = "Paper";
+        computerChoice = "Paper";
     }else{
-        compChoice = "Scissors";
+        computerChoice = "Scissors";
      }
-     return compChoice;
+     return computerChoice;
 }
 
-let test = getComputerChoice();
+// Function to determine who wins
 
-console.log(test);
+function playRound(playerSelection, computerSelection){
+
+    //standardise case from playerSelection
+    let playerChoice = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    
+    // Stores who will win
+    let outcome;
+
+    //Logic for the game
+    if (playerChoice === "Rock" && computerSelection === "Paper"
+        || playerChoice === "Paper" && computerSelection === "Scissors"
+        || playerChoice === "Scissors" && computerSelection === "Rock" )
+        {
+        outcome = "You Lose! " + computerSelection + " beats " + playerChoice;
+    }
+    else if (playerChoice === computerSelection){
+        outcome = "You Draw! The Player's choice (" + playerChoice + ") and Computer's choice (" + computerSelection + ") are the same!";
+    }
+    else {
+        outcome = "You Win! " + playerChoice + " beats " + computerSelection;
+    }
+    //Return outcome of game
+    return outcome;
+}
+
+let computerRPS = getComputerChoice();
+
+console.log(playRound("rOck", "Scissors"));
